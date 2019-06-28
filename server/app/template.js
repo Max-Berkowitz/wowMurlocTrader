fc79f0874a137f872c5d53f89e6fb535ab807dfa
Max Berkowitz <maxl.berkowitz@gmail.com>
2019-06-27 17:50:59 -0700

    changed title and description on template

--- server/app/template.js
+++ server/app/template.js
@@ -4,9 +4,9 @@ export default html =>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="Description" content="A practice space for isomorphic web application architecture">
+        <meta name="Description" content="Help with the wow murloc traders">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Isomorphic Practice</title>
+        <title>Wow Murloc Trader</title>
     </head>
     <body>
         <div id="app">${html}</div>
